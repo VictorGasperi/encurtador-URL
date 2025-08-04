c4a206db9e48540710a204fb5fbef3d5d8a36f95
Victor Gasperi <victorgasperi@gmail.com>
2025-08-04 00:56:40 -0300

    refactor: fazer com que tenha multiplos contextos no deploy

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket = "gasp-terraform-backend-state"
-    key    = "url_shortener/terraform.tfstate"
-    region = "us-east-2"
-    encrypt = true
-  }
+  backend "s3" {}
 }
