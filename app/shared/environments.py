a6d6332ebd0ef26f8518054512d2b620733a1ecb
Victor Gasperi <victorgasperi@gmail.com>
2025-08-05 17:05:00 -0300

    fix: puxando stage do objeto correto

--- app/shared/environments.py
+++ app/shared/environments.py
@@ -48,6 +48,6 @@ class Environments:
         envs = Environments()
         envs.load_envs()
         print('A MAGIA DO PRINT!!!!!!')
-        print(Environments.stage)
-        print(Environments.dynamo_table_name)
+        print(envs.stage)
+        print(envs.dynamo_table_name)
         return envs
